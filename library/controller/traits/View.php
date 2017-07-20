7a2ad3a4f3f5426ddd37f7cbda59707a02b7a83f
hdj <hdj@addcn.com>
2017-07-20 13:52:32 +0800

    修正Exception

--- library/controller/traits/View.php
+++ library/controller/traits/View.php
@@ -23,7 +23,7 @@ class View
     public function __call($name, $args)
     {
         Response::instance()->sendHttpStatus(404);
-        throw new Exception(get_called_class() . "::{$name}() IS NOT EXISTS", 1);
+        throw new \Exception(get_called_class() . "::{$name}() IS NOT EXISTS", 1);
     }
 
     protected function fetch($templateFile = '')
