011a3acfddde65810a5792aadc7eabbbd4d2855c
hdj <hdj@addcn.com>
2017-09-13 08:55:29 +0800

    增加控制器构造方法注入容器功能

--- library/App.php
+++ library/App.php
@@ -1,6 +1,9 @@
 <?php
 namespace deeka;
 
+use Exception;
+use ReflectionException;
+
 class App
 {
     private function __construct()
@@ -65,11 +68,11 @@ class App
         // 检查控制器名合法性
         if (!preg_match('/^[A-Za-z](\w)*$/', CONTROLLER_NAME)) {
             $error = "Controller does not exists:" . CONTROLLER_NAME;
-            throw new \Exception($error);
+            throw new Exception($error);
         }
         if (!preg_match('/^[A-Za-z](\w)*$/', ACTION_NAME)) {
             $error = "Action does not exists:" . ACTION_NAME;
-            throw new \Exception($error);
+            throw new Exception($error);
         }
         // [app.namespace]\[module]\[controller]\[controller_name]
         $controller_name = sprintf(
@@ -80,75 +83,29 @@ class App
             CONTROLLER_NAME
         );
         $action_name = ACTION_NAME . Config::get('layer.action', '');
-        // 加载控制器
-        $controller = Loader::controller($controller_name);
         // 加载失败则抛出异常
-        if (false === $controller) {
+        if (!class_exists($controller_name)) {
             $error = "Controller does not exists: '{$controller_name}'";
-            throw new \Exception($error, 1);
+            throw new Exception($error, 1);
         }
-        // 参数绑定
+        // 参数绑定类型
+        $bind_type = Config::get('app.url_params_bind_type', 0);
+        // 加载控制器
         try {
-            // 参数绑定类型 0 = 变量名, 1 = 顺序
-            self::invokeMethod([$controller, $action_name], Input::param(), Config::get('app.url_params_bind_type', 0));
-        } catch (\ReflectionException $e) {
+            $controller = Reflect::invokeClass($controller_name, Input::param(), $bind_type);
+        } catch (ReflectionException $e) {
+            throw new Exception("{$controller_name} instance faild", 1);
+        }
+        // 参数绑定, 参数绑定类型 0 = 变量名, 1 = 顺序
+        try {
+            Reflect::invokeMethod([$controller, $action_name], Input::param(), $bind_type);
+        } catch (ReflectionException $e) {
             try {
-                self::invokeMethod([$controller, '__call'], [$action_name, Input::param()], 1);
-            } catch (\ReflectionException $e) {
-                throw new \Exception("Error action:{$action_name}");
+                Reflect::invokeMethod([$controller, '__call'], [$action_name, Input::param()], 1);
+            } catch (ReflectionException $e) {
+                throw new Exception("Error action:{$action_name}");
             }
         }
         return;
-    }
-
-    /**
-     * @param $method 发射方法
-     * @param array $vars 参数
-     * @param $bind_type 参数绑定类型 0 = 变量名, 1 = 顺序
-     * @return mixed
-     */
-    public static function invokeMethod($method, $vars = [], $bind_type = 0)
-    {
-        if (is_array($method)) {
-            $object  = is_object($method[0]) ? $method[0] : new $method[0]();
-            $reflect = new \ReflectionMethod($object, $method[1]);
-        } else {
-            $reflect = new \ReflectionMethod($method);
-        }
-        $args = self::bindParams($reflect, $vars, $bind_type);
-        return $reflect->invokeArgs($object ?? null, $args);
-    }
-
-    /**
-     * @param $reflect 反射对象
-     * @param array $vars 参数
-     * @param $bind_type 参数绑定类型 0 = 变量名, 1 = 顺序
-     * @return mixed
-     */
-    public static function bindParams($reflect, $vars = [], $bind_type = 0)
-    {
-        $vars = $vars ?? Input::param();
-        $args = [];
-        if ($reflect->getNumberOfParameters() > 0) {
-            // 判断数组类型 数字数组时按顺序绑定参数
-            $params = $reflect->getParameters();
-            foreach ($params as $param) {
-                $name  = $param->getName();
-                $class = $param->getClass();
-                if ($class) {
-                    $cn     = $class->getName();
-                    $args[] = method_exists($cn, 'instance') ? $cn::instance() : new $cn;
-                } elseif (1 == $bind_type && !empty($vars)) {
-                    $args[] = array_shift($vars);
-                } elseif (0 == $bind_type && isset($vars[$name])) {
-                    $args[] = $vars[$name];
-                } elseif ($param->isDefaultValueAvailable()) {
-                    $args[] = $param->getDefaultValue();
-                } else {
-                    throw new \Exception('Error param:' . $name);
-                }
-            }
-        }
-        return $args;
     }
 }
