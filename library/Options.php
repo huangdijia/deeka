8b9e5b6c7f53d2c6f597fa49969f689bb29b943c
hdj <hdj@addcn.com>
2017-09-19 08:34:45 +0800

    优化Options，支持控制器依赖注入

--- library/Options.php
+++ library/Options.php
@@ -3,6 +3,16 @@ namespace deeka;
 
 class Options
 {
+    protected static $instance = null;
+
+    public static function instance()
+    {
+        if (is_null(self::$instance)) {
+            self::$instance = new static;
+        }
+        return self::$instance;
+    }
+
     private function __construct()
     {
         //
@@ -13,23 +23,44 @@ class Options
         //
     }
 
-    public static function get(string $name = '', $default = null)
+    public function __call($name, $args)
+    {
+        return call_user_func_array([self::instance(), $name], $args);
+    }
+
+    public static function __callStatic($name, $args)
+    {
+        return call_user_func_array([self::instance(), $name], $args);
+    }
+
+    private function get(string $name = '', $default = null)
+    {
+        if ('' == $name) {
+            return self::all();
+        } else {
+            return self::all()[$name] ?? $default;
+        }
+    }
+
+    private function all()
     {
         static $argv = null;
         if (is_null($argv)) {
-            [$shortopts, $longopts] = self::parse();
+            [$shortopts, $longopts] = $this->parse();
             $argv                   = getopt($shortopts, $longopts);
         }
-        if ('' == $name) {
-            return $argv;
-        }
-        return $argv[$name] ?? $default;
+        return $argv;
     }
 
-    private static function parse(): array
+    private function has($name = '')
+    {
+        return isset(self::all()[$name]) ? true : false;
+    }
+
+    private function parse(): array
     {
         if (empty($_SERVER['argv'])) {
-            return [];
+            return $opts = [];
         }
         $opts = ['', []];
         foreach ($_SERVER['argv'] as $argv) {
