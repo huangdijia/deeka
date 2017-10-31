01e4b74897231579b334085b900b533feb3bf728
hdj <hdj@addcn.com>
2017-10-31 15:45:25 +0800

    增加Response::emptyToNull()

--- library/response/Json.php
+++ library/response/Json.php
@@ -7,18 +7,19 @@ use deeka\Log;
 class Json extends Response
 {
     protected $options = [
-        'json_encode_param' => JSON_UNESCAPED_UNICODE,
+        'json_encode_param'  => JSON_UNESCAPED_UNICODE,
+        'json_empty_to_null' => false,
     ];
 
     public function render($data)
     {
+        if ($this->options['json_empty_to_null']) {
+            $data = self::emptyToNull($data);
+        }
         $json = json_encode($data, $this->options['json_encode_param']);
         if (\json_last_error()) {
-            Log::record(\json_last_error_msg(), Log::ERR);
+            Log::record(\json_last_error_msg());
         }
-        // fix json php for java
-        $json = str_replace(['[]', '{}', '""'], ['null', 'null', 'null'], $json);
         return $json;
     }
-
 }
