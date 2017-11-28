79865f2e34b37ad0b4f9667dfb4deb340f362844
hdj <hdj@addcn.com>
2017-11-28 13:08:13 +0800

    兼容psr16

--- library/cache/driver/Redis.php
+++ library/cache/driver/Redis.php
@@ -54,7 +54,12 @@ class Redis extends Cache implements CacheInterface
 
     public function getMultiple($keys, $default = null)
     {
-        return $this->handler->mGet($keys);
+        // return $this->handler->mGet($keys);
+        $resp = [];
+        foreach ((array) $keys as $key) {
+            $resp[$key] = $this->get($key) ?? $default;
+        }
+        return $resp;
     }
 
     public function setMultiple($values, $ttl = null)
